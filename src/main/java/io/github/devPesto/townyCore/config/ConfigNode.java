7094e45682ead72b8075d301680e59b21b8c5080
_Pesto <pestonotpasta@gmail.com>
2024-08-16 06:22:23 -0700

    Update config options

--- src/main/java/io/github/devPesto/townyCore/config/ConfigNode.java
+++ src/main/java/io/github/devPesto/townyCore/config/ConfigNode.java
@@ -3,7 +3,9 @@ package io.github.devPesto.townyCore.config;
 import lombok.Getter;
 
 public enum ConfigNode {
-	EXPANSION_ENABLE_MINER_KIT("expansions.enable-miner-kit")
+	EXPANSION_ENABLE_MINER_KIT("expansions.enable-miner-kit"),
+	EXPANSION_ENABLE_OC_SOUNDS("expansions.enable-old-combat-sounds"),
+	EXPANSION_ENABLE_RALLIES("expansions.lunar-integrations.enable-rallies"),
 	;
 
 	private final @Getter String path;
