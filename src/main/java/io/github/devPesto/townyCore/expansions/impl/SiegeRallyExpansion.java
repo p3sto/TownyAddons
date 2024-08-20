d01443c300c8b427a05bb6129be35a7839e59e3c
_Pesto <pestonotpasta@gmail.com>
2024-08-19 18:51:05 -0700

    Add event listener removal

--- src/main/java/io/github/devPesto/townyCore/expansions/impl/SiegeRallyExpansion.java
+++ src/main/java/io/github/devPesto/townyCore/expansions/impl/SiegeRallyExpansion.java
@@ -5,13 +5,16 @@ import io.github.devPesto.townyCore.commands.SiegeRallyCommand;
 import io.github.devPesto.townyCore.expansions.TownyExpansion;
 import io.github.devPesto.townyCore.listener.SiegeRallyListener;
 import io.github.devPesto.townyCore.manager.SiegeRallyManager;
+import org.bukkit.event.HandlerList;
 
 public class SiegeRallyExpansion extends TownyExpansion {
     private final SiegeRallyManager manager;
+    private final SiegeRallyListener listener;
 
     public SiegeRallyExpansion() {
         super("Rallies", "Apollo-Bukkit", "SiegeWar");
         this.manager = new SiegeRallyManager();
+        this.listener = new SiegeRallyListener(manager);
     }
 
     @Override
@@ -20,8 +23,18 @@ public class SiegeRallyExpansion extends TownyExpansion {
     }
 
     @Override
+    protected void registerListeners(TownyCore plugin) {
+        plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+    }
+
+    @Override
     public void register(TownyCore plugin) {
         super.register(plugin);
         manager.registerRallies();
+    }
+
+    @Override
+    protected void unregisterListeners(TownyCore plugin) {
+        HandlerList.unregisterAll(listener);
     }
 }
